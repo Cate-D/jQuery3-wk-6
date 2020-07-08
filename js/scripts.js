34d260f66bbdecdd7fc68e18c3fbd7a9ee353866
Cate Draney <catiedraney@gmail.com>
2020-07-07 17:12:59 -0700

    added drinks menu.

--- js/scripts.js
+++ js/scripts.js
@@ -1,21 +1,12 @@
 $(document).ready(function() {
-  $("#formOne").submit(function(event) {
-    const person1Input = $("input#person1").val();
-    const person2Input = $("input#person2").val();
-    const animalInput= $("input#animal").val();
-    const exclamationInput = $("input#exclamation").val();
-    const verbInput = $("input#verb").val();
-    const nounInput = $("input#noun").val();
+  const age = parseInt(prompt("How old are you?"));
 
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
-
-    $("#story").show();
-
-    event.preventDefault();
-  });
+  if (age > 21) {
+    $('#drinks').show();
+  } else if (age === 21) {
+    alert("Now don't go crazy!");
+    $('#drinks').show();
+  } else {
+    $('#under-21').show();
+  }
 });
